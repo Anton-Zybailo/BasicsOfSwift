b21a83ae8c6e0153d41ed7f82d8676db90c073c1
Anton-Zybailo <anton_zybailo@mail.ru>
2018-12-18 23:48:01 +0300

    Fix homework №4

--- 4l_ZybailoAnton.playground/Contents.swift
+++ 4l_ZybailoAnton.playground/Contents.swift
@@ -1,48 +1,48 @@
 import UIKit
 // Task № 1, 2, 3, 4, 5, 6
 
-enum CarsEngineState {
+enum CarEngineState {
     case on, off
 }
-enum CarsDoorState {
+enum CarDoorState {
     case open, close
 }
-enum CarsTrunkVolume {
+enum CarTrunkVolume {
     case empty, full
 }
 // General class
-class Cars {
-    let carsBrand: String
-    let carsCreationYear: Int
-    var carsPower: Double
-    var carsEngineState: CarsEngineState
-    var carsDoorState: CarsDoorState
-    var carsTrunkVolume: CarsTrunkVolume
+class Car {
+    let brand: String
+    let creationYear: Int
+    var power: Double
+    var engineState: CarEngineState
+    var doorState: CarDoorState
+    var trunkVolume: CarTrunkVolume
     static var carCount = 0
     
-    init (carsBrand: String, carsCreationYear: Int, carsPower: Double, carsEngineState: CarsEngineState, carsDoorState: CarsDoorState, carsTrunkVolume: CarsTrunkVolume) {
-        self.carsBrand = carsBrand
-        self.carsCreationYear = carsCreationYear
-        self.carsPower = carsPower
-        self.carsEngineState = carsEngineState
-        self.carsDoorState = carsDoorState
-        self.carsTrunkVolume = carsTrunkVolume
-        Cars.carCount += 1
+    init (brand: String, creationYear: Int, power: Double, engineState: CarEngineState, doorState: CarDoorState, trunkVolume: CarTrunkVolume) {
+        self.brand = brand
+        self.creationYear = creationYear
+        self.power = power
+        self.engineState = engineState
+        self.doorState = doorState
+        self.trunkVolume = trunkVolume
+        Car.carCount += 1
     }
     static func countInfo () {
         print ("Total cars: \(self.carCount)")
     }
     func openDoorState() {
-        carsDoorState = .open
+        doorState = .open
     }
     func closeDoorState() {
-        carsDoorState = .close
+        doorState = .close
     }
     func infoCarsPower() {
-        if carsPower >= 3 {
-            print ("Powerful car! Car's power: \(carsPower)")
+        if power >= 3 {
+            print ("Powerful car! Car's power: \(power)")
         } else {
-            print ("Normal power. Car's power: \(carsPower)")
+            print ("Normal power. Car's power: \(power)")
         }
     }
 }
@@ -51,24 +51,24 @@ class Cars {
 enum AutomotiveAxles {
     case four, six, eight
 }
-class TrunkCars: Cars {
+class TrunkCars: Car {
     var tonnage: Double
     var additionalTrailer: Bool
     var autumotiveAxles: AutomotiveAxles
     
-    init (carsBrand: String, carsCreationYear: Int, carsPower: Double, carsEngineState: CarsEngineState, carsDoorState: CarsDoorState, carsTrunkVolume: CarsTrunkVolume, tonnage: Double, additionalTrailer: Bool, autumotiveAxles: AutomotiveAxles) {
+    init (brand: String, creationYear: Int, power: Double, engineState: CarEngineState, doorState: CarDoorState, trunkVolume: CarTrunkVolume, tonnage: Double, additionalTrailer: Bool, autumotiveAxles: AutomotiveAxles) {
         
         self.tonnage = tonnage
         self.additionalTrailer = additionalTrailer
         self.autumotiveAxles = autumotiveAxles
-        super.init (carsBrand: carsBrand, carsCreationYear: carsCreationYear, carsPower: carsPower, carsEngineState: carsEngineState, carsDoorState: carsDoorState, carsTrunkVolume: carsTrunkVolume)
+        super.init (brand: brand, creationYear: creationYear, power: power, engineState: engineState, doorState: doorState, trunkVolume: trunkVolume)
     }
     override func infoCarsPower() {
         super.infoCarsPower()
-        if carsPower >= 4 {
-            print ("Powerful car! Car's power: \(carsPower)")
+        if power >= 4 {
+            print ("Powerful car! Car's power: \(power)")
         } else {
-            print ("Normal power. Car's power: \(carsPower)")
+            print ("Normal power. Car's power: \(power)")
         }
     }
 }
@@ -80,65 +80,65 @@ enum Transmission {
 enum Generation {
     case first, second, third
 }
-class SportCars: Cars {
+class SportCars: Car {
 
     var accelerationTo100: Double
     var transmission: Transmission
     var maxSpeed: Int
     var generation: Generation
     
-    init (carsBrand: String, carsCreationYear: Int, carsPower: Double, carsEngineState: CarsEngineState, carsDoorState: CarsDoorState, carsTrunkVolume: CarsTrunkVolume, accelerationTo100: Double, transmission: Transmission, maxSpeed: Int, generation: Generation) {
+    init (brand: String, creationYear: Int, power: Double, engineState: CarEngineState, doorState: CarDoorState, trunkVolume: CarTrunkVolume, accelerationTo100: Double, transmission: Transmission, maxSpeed: Int, generation: Generation) {
        
         self.accelerationTo100 = accelerationTo100
         self.transmission = transmission
         self.maxSpeed = maxSpeed
         self.generation = generation
-        super.init(carsBrand: carsBrand, carsCreationYear: carsCreationYear, carsPower: carsPower, carsEngineState: carsEngineState, carsDoorState: carsDoorState, carsTrunkVolume: carsTrunkVolume)
+        super.init (brand: brand, creationYear: creationYear, power: power, engineState: engineState, doorState: doorState, trunkVolume: trunkVolume)
     }
     override func infoCarsPower() {
         super.infoCarsPower()
-        if carsPower >= 6 {
-            print ("Powerful car! Car's power: \(carsPower)")
+        if power >= 6 {
+            print ("Powerful car! Car's power: \(power)")
         } else {
-            print ("Normal power. Car's power: \(carsPower)")
-        }
-        func openDoorState() {                                  // Fix world "override"
-        carsDoorState = .close
-        print ("Doors are always close")
+            print ("Normal power. Car's power: \(power)")
         }
     }
+    override func openDoorState() {
+        doorState = .close
+        print ("Doors are always close")
+        }
 }
 
-var manTrunk = TrunkCars (carsBrand: "Man 6x6", carsCreationYear: 2009, carsPower: 4, carsEngineState: .on, carsDoorState: .open, carsTrunkVolume: .empty, tonnage: 15, additionalTrailer: false, autumotiveAxles: .four)
+var manTrunk = TrunkCars (brand: "Man 6x6", creationYear: 2009, power: 4, engineState: .on, doorState: .open, trunkVolume: .empty, tonnage: 15, additionalTrailer: false, autumotiveAxles: .four)
 
-var volvoTrunk = TrunkCars (carsBrand: "Volvo Truck", carsCreationYear: 2015, carsPower: 6, carsEngineState: .on, carsDoorState: .close, carsTrunkVolume: .full, tonnage: 24, additionalTrailer: true, autumotiveAxles: .six)
+var volvoTrunk = TrunkCars (brand: "Volvo Truck", creationYear: 2015, power: 6, engineState: .on, doorState: .close, trunkVolume: .full, tonnage: 24, additionalTrailer: true, autumotiveAxles: .six)
 
-var porsheSport = SportCars (carsBrand: "Porshe 911", carsCreationYear: 2018, carsPower: 6, carsEngineState: .on, carsDoorState: .close, carsTrunkVolume: .empty, accelerationTo100: 5.7, transmission: .automatic, maxSpeed: 240, generation: .third)
+var porsheSport = SportCars (brand: "Porshe 911", creationYear: 2018, power: 6, engineState: .on, doorState: .close, trunkVolume: .empty, accelerationTo100: 5.7, transmission: .automatic, maxSpeed: 240, generation: .third)
 
-var mersedesSport = SportCars (carsBrand: "Mersedes S-class AMG", carsCreationYear: 2015, carsPower: 8, carsEngineState: .on, carsDoorState: .close, carsTrunkVolume: .empty, accelerationTo100: 4.9, transmission: .automatic, maxSpeed: 280, generation: .second)
+var mersedesSport = SportCars (brand: "Mersedes S-class AMG", creationYear: 2015, power: 8, engineState: .on, doorState: .close, trunkVolume: .empty, accelerationTo100: 4.9, transmission: .automatic, maxSpeed: 280, generation: .second)
 
-var gazelTrunk = TrunkCars (carsBrand: "Gazel Next", carsCreationYear: 2014, carsPower: 2, carsEngineState: .on, carsDoorState: .open, carsTrunkVolume: .full, tonnage: 8, additionalTrailer: false, autumotiveAxles: .four)
+var gazelTrunk = TrunkCars (brand: "Gazel Next", creationYear: 2014, power: 2, engineState: .on, doorState: .open, trunkVolume: .full, tonnage: 8, additionalTrailer: false, autumotiveAxles: .four)
 
-var audiSport = SportCars (carsBrand: "Audi RS-8", carsCreationYear: 2016, carsPower: 8, carsEngineState: .on, carsDoorState: .close, carsTrunkVolume: .empty, accelerationTo100: 4.1, transmission: .automatic, maxSpeed: 300, generation: .second)
+var audiSport = SportCars (brand: "Audi RS-8", creationYear: 2016, power: 8, engineState: .on, doorState: .close, trunkVolume: .empty, accelerationTo100: 4.1, transmission: .automatic, maxSpeed: 300, generation: .second)
 
-var teslaSport = SportCars (carsBrand: "Tesla Space-X", carsCreationYear: 2018, carsPower: 2, carsEngineState: .on, carsDoorState: .close, carsTrunkVolume: .empty, accelerationTo100: 3.9, transmission: .automatic, maxSpeed: 220, generation: .first)
+var teslaSport = SportCars (brand: "Tesla Space-X", creationYear: 2018, power: 2, engineState: .on, doorState: .close, trunkVolume: .empty, accelerationTo100: 3.9, transmission: .automatic, maxSpeed: 220, generation: .first)
 
-print ("All cars: \(Cars.carCount)")
+print ("All cars: \(Car.carCount)")
 
-print ("Tonnage: 1.\(manTrunk.carsBrand): \(manTrunk.tonnage), 2. \(volvoTrunk.carsBrand): \(volvoTrunk.tonnage), 3. \(gazelTrunk.carsBrand): \(gazelTrunk.tonnage).")
+print ("Tonnage: 1.\(manTrunk.brand): \(manTrunk.tonnage), 2. \(volvoTrunk.brand): \(volvoTrunk.tonnage), 3. \(gazelTrunk.brand): \(gazelTrunk.tonnage).")
 
 mersedesSport.infoCarsPower()
 teslaSport.infoCarsPower()
 
-print ("Acceleration to 100: 1. \(porsheSport.carsBrand): \(porsheSport.accelerationTo100)sec; 2. \(mersedesSport.carsBrand): \(mersedesSport.accelerationTo100)sec; 3. \(audiSport.carsBrand): \(audiSport.accelerationTo100)sec; 4. \(teslaSport.carsBrand): \(teslaSport.accelerationTo100)sec. ")
+print ("Acceleration to 100: 1. \(porsheSport.brand): \(porsheSport.accelerationTo100)sec; 2. \(mersedesSport.brand): \(mersedesSport.accelerationTo100)sec; 3. \(audiSport.brand): \(audiSport.accelerationTo100)sec; 4. \(teslaSport.brand): \(teslaSport.accelerationTo100)sec. ")
 
 manTrunk.tonnage = 17
 mersedesSport.accelerationTo100 = 3.8
 teslaSport.accelerationTo100 = 2.5
 audiSport = porsheSport
 
-print ("All cars: \(Cars.carCount)")
+print ("All cars: \(Car.carCount)")
 
-print ("Tonnage: 1.\(manTrunk.carsBrand): \(manTrunk.tonnage), 2. \(volvoTrunk.carsBrand): \(volvoTrunk.tonnage), 3. \(gazelTrunk.carsBrand): \(gazelTrunk.tonnage).")
+print ("Tonnage: 1.\(manTrunk.brand): \(manTrunk.tonnage), 2. \(volvoTrunk.brand): \(volvoTrunk.tonnage), 3. \(gazelTrunk.brand): \(gazelTrunk.tonnage).")
 
-print ("Acceleration to 100: 1. \(porsheSport.carsBrand): \(porsheSport.accelerationTo100)sec; 2. \(mersedesSport.carsBrand): \(mersedesSport.accelerationTo100)sec; 3. \(audiSport.carsBrand): \(audiSport.accelerationTo100)sec; 4. \(teslaSport.carsBrand): \(teslaSport.accelerationTo100)sec. ")
+print ("Acceleration to 100: 1. \(porsheSport.brand): \(porsheSport.accelerationTo100)sec; 2. \(mersedesSport.brand): \(mersedesSport.accelerationTo100)sec; 3. \(audiSport.brand): \(audiSport.accelerationTo100)sec; 4. \(teslaSport.brand): \(teslaSport.accelerationTo100)sec. ")
